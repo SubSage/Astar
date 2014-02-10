817246a3237842f9baff9df89cd40081ceb6ba2b
Adan Rodriguez <mario2485@yahoo.com>
2014-02-10 17:42:31 -0600

    Update graph.h
    Fixed annoying syntax errors and for loops

--- project/graph.h
+++ project/graph.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 class weightedGraph
@@ -14,77 +16,89 @@ public:
 
     class Vertex
     {
-		public:
-		string locationName;
-		double g;
-		double h;
-		double f;
-		Vertex* parent;
-		list<Edge*> adjList;
+    public:
+	string locationName;
+	double g;
+	double h;
+	double f;
+	Vertex* parent;
+	list<Edge*> adjList;
 		
-		Vertex(){
-			g=0;
-			h=0;
-			f=0;
-			locationName="";
+	Vertex()
+	{
+	    g=0;
+	    h=0;
+	    f=0;
+	    locationName="";
 		
-		}
-		Vertex(string name, double h2)
+	}
+	Vertex(string name, double h2)
+	{
+	    locationName = name;
+	    h = h2;
+	}
+
+	bool operator<(const Vertex & b)
+	{
+	    return (h > b.h);
+	}
+
+	vector<Vertex*> getNeighbors()
+	{
+	    vector<Vertex*> p;
+	    for ( list<Edge*>::iterator it = adjList.begin(); it != adjList.end(); it++ )
+	    {
+		if(locationName == (*it)->start->locationName)
 		{
-			locationName = name;
-			h = h2;
-		}
-
-		bool operator<(const Vertex & b){
-			return h > b.h;
-		}
-
-		vector<weightedGraph::Vertex*> getNeighbors(){
-			vector<weightedGraph::Vertex*> p;
-			for each(Edge * e in adjList)
-			{
-				p.push_back(e->getEnd(locationName));
-			}
-			return p;
-		}
+		    p.push_back( (*it)->end );
+		} 
+	        if (locationName == (*it)->end->locationName)
+		    p.push_back( (*it)->start );
+                
+	    }
+	    return p;
+	}
     };
 
     class Edge
     {
-		public:
-		Vertex* start;
-		Vertex* end;
-		double distance;
-		double roadQuality;
-		double riskLevel;
+    public:
+	Vertex* start;
+	Vertex* end;
+	double distance;
+	double roadQuality;
+	double riskLevel;
 
-		Edge(Vertex* u, Vertex* v, double distance2, double roadQuality2, double riskLevel2)
-		{
-			start = u;
-			end = v;
-			distance = distance2;
-			roadQuality = roadQuality2;
-			riskLevel = riskLevel2;
-		}
+	Edge(Vertex* u, Vertex* v, double distance2, double roadQuality2, double riskLevel2)
+	{
+	    start = u;
+	    end = v;
+	    distance = distance2;
+	    roadQuality = roadQuality2;
+	    riskLevel = riskLevel2;
+	}
 
-		Vertex* getEnd(string a){
-			if(a==start->locationName){
-				return end;
-			} else if(a==end->locationName)
-				return start;
-			return NULL;
-		}
+	Vertex* getEnd(string a)
+	{
+	    if(a == start->locationName)
+	    {
+		return end;
+	    } 
+	    else if (a == end->locationName)
+		return start;
+	    return NULL;
+	}
     };
 
     list<Vertex*> vertexList;
 
     Vertex * findVertex(string name)
     {
-		for each(Vertex * v in vertexList)
-		{
-			if (v->locationName == name)
-				return v;
-		}
+	for (list<Vertex*>::iterator it = vertexList.begin(); it != vertexList.end(); it++ )
+	{
+	    if ( (*it)->locationName == name)
+		return (*it);
+	}
 	return NULL;
     }
     
@@ -94,20 +108,20 @@ public:
 
     void addVertex(string name, double h)
     {
-		vertexList.push_back( new Vertex(name, h) );
+	vertexList.push_back( new Vertex(name, h) );
     }
 
     void addEdge(string a, string b, double distance, double roadQuality, double riskLevel)
     {
-		Vertex* vertA = findVertex(a);
-		Vertex* vertB = findVertex(b);
+	Vertex* vertA = findVertex(a);
+	Vertex* vertB = findVertex(b);
 
-		// Create edge
-		Edge* path = new Edge(vertA, vertB, distance, roadQuality, riskLevel);
+	// Create edge
+	Edge* path = new Edge(vertA, vertB, distance, roadQuality, riskLevel);
 
-		// Add bidirectional edge between the vertA and vertB
-		vertA->adjList.push_back(path);
-		vertB->adjList.push_back(path);
+	// Add bidirectional edge between the vertA and vertB
+	vertA->adjList.push_back(path);
+	vertB->adjList.push_back(path);
     }
 
 };
