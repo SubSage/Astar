2b3152d398f52236e38b138f615318f3944ef61d
Adan Rodriguez <mario2485@yahoo.com>
2014-02-10 17:43:35 -0600

    Update main.cpp
    Updated, parsing still needs to be done

--- project/main.cpp
+++ project/main.cpp
@@ -28,93 +28,109 @@ void parseInput(ifstream& inputFile, string dataAction, weightedGraph& graph)
 {
     string data;
     string name;
-    double value=0;
+    double value = 0;
     vector<string> lineParts;
     string constructedLine;
     
     // Loads Table 1 file to build nodes with data
     if (dataAction == "buildNodes")
     {
-		inputFile.open("dist_to_iron_hills.txt");
+	string name;
+	double value;
+	vector<string> lineParts;
+	string constructedLine;
 	
-		while ( getline(inputFile, data) )
+	inputFile.open("dist_to_iron_hills.txt");
+	
+	while ( getline(inputFile, data) )
+	{
+	    constructedLine = "";
+	    for (int i = 0; i < data.size(); i++)
+	    {
+		if (data[i] != '-')
+		    constructedLine += data[i];
+		else
 		{
+		    if (constructedLine != "")
+		    {
+			lineParts.push_back(constructedLine);
 			constructedLine = "";
-			for (int i = 0; i < data.size(); i++)
-			{
-				if (data[i] != '-')
-					constructedLine += data[i];
-				else
-				{
-					if (constructedLine != "")
-					{
-						lineParts.push_back(constructedLine);
-						constructedLine = "";
-					}
-				}
-			}
-			lineParts.push_back(constructedLine); // Adds distance value from Table 1
+		    }
+		}
+	    }
+	    lineParts.push_back(constructedLine); // Adds distance value from Table 1
 	    
-			name = lineParts[0];
-			value = atof(lineParts[1].c_str());
-
-			// Build nodes here
-			buildNode(name, value, graph);
-		}	
+	    name = lineParts[0];
+	    value = atof(lineParts[1].c_str());
+	    
+	    // Build nodes here
+	    buildNode(name, value, graph);
+	}	
 	
 	
     }
-
+    
     // Loads Table 2 file with point to point information (Edges) and build edges.
     // between the created Nodes
     if (dataAction == "buildEdges")
     {
-		inputFile.open("point_to_point.txt");
-
-		// ******* Parse code for build WORK IN PROGRESS *******
-		while ( getline(inputFile, data) )
-		{
-
-				// See if line contains a : to extract start node name, otherwise check next condition
-			if ( data.find(':') != string::npos )
-			{
-				constructedLine = "";
-				for (int i = 0; i < data.size(); i++)
-				{
-					// Get name, take away the colon
-		    
-				}
-
-				// DEBUG
-				cout << "Found :" << endl;
-			}	
-			// See if line contains a - to see if it's a node path
-			else if ( data.find('-') != string::npos )
-			{
-				constructedLine = "";
-				for (int i = 0; i < data.size(); i++)
-				{
-		    
-		    
-				}
-
-				// DEBUG
-				cout << "Found -" << endl;
-				}
-	    
-				// Line is blank
-				else
-				{// DEBUG
-				cout << "Found blank line" << endl;
-
-				continue;		
-				}
-		}
-    }      
+	string locationOneName;
+	string locationTwoName;
+	double pathDistance;
+	double pathRoadQuality;
+	double pathRiskLevel;
+	vector<string> lineParts;
+	string constructedLine;
 	
+	inputFile.open("point_to_point.txt");
+	
+	// ******* Parse code for build WORK IN PROGRESS *******
+	while ( getline(inputFile, data) )
+	{
+	    
+	    // See if line contains a : to extract start node name, otherwise check next condition
+	    if ( data.find(':') != string::npos )
+	    {
+		int pos;  // Store position of colon
+		
+		constructedLine = "";
+		
+		// Subtract 1 from data.size() to exclude colon
+		for (int i = 0; i < data.size() - 1; i++)
+		{
+		    // Get name
+		    constructedLine += data[i];
+		}
+		
+		// DEBUG
+		cout << "Found :" << endl;
+	    }	
+	    // See if line contains a - to see if it's a node path
+	    else if ( data.find('-') != string::npos )
+	    {
+		constructedLine = "";
+		for (int i = 0; i < data.size(); i++)
+		{
+		       
+		}
+		
+		// DEBUG
+		cout << "Found -" << endl;
+	    }
+	    
+	    // Line is blank
+	    else
+	    {// DEBUG
+		cout << "Found blank line" << endl;
+		
+		continue;		
+	    }
+	}
+    }      
+    
     // DEBUG
     cout << "Name: \"" << name << "\" Value: " << value << endl;
-        
+    
     lineParts.clear();
     inputFile.close();
 }
